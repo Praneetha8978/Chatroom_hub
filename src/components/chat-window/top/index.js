bfaaff01f2bf16590907f9565025da7a7995b897
praneetha devi mudunuri <20b01a12b3@svecw.edu.in>
2023-12-06 00:27:45 +0530

    created roomInfo link for chat top

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,11 +1,29 @@
 import React,{memo} from 'react'
 import { useCurrentRoom } from '../../../context/current-room.context'
+import ArrowLeftIcon from '@rsuite/icons/ArrowLeft';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar } from 'rsuite';
+import '../../../styles/main.scss';
+import RoomInfoBtnModel from './RoomInfoBtnModel';
 
 const Top = () => {
   const name = useCurrentRoom(v => v.name);
+  const isMobile = window.innerWidth <= 767
   return (
     <div>
-      {name}
+      <div className='d-flex justify-content-between align-items-center'>
+        <h4>
+          <Link to="/" className={isMobile ? 'd-inline-block p-0 mr-2 text-blue link-unstyled':'d-none'}><ArrowLeftIcon color='blue'/></Link>
+          <span className='text-disappear'>{name}</span>
+        </h4>
+        <ButtonToolbar>
+          <span>todo</span>
+        </ButtonToolbar>
+      </div>
+      <div className='d-flex justify-content-between align-items-center'>
+        <span>todo</span>
+        <RoomInfoBtnModel/>
+      </div>
     </div>
   )
 }
